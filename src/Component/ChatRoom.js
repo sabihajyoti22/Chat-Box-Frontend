7cf7f8b3aa5f77d6ba1222ac7645454482be2426
sabihajyoti22 <sabihajyoti@gmail.com>
2024-11-06 21:03:21 +0600

    chore: remove options

--- src/Component/ChatRoom.js
+++ src/Component/ChatRoom.js
@@ -16,10 +16,7 @@ export default function ChatRoom() {
     const [allMessages, setAllMessages] = useState([])
     
     useEffect(()=>{
-        const socket = io(process.env.REACT_APP_SERVER_URL, {
-            transports: ['websocket'],
-            withCredentials: true
-        })
+        const socket = io(process.env.REACT_APP_SERVER_URL)
         setScoket(socket)
         
         socket.on("connect", () => {
